8136c643ebb2afd63eb1f41b0851b20205cbdbd1
Emil <emil.sivervik@kit.se>
2019-02-08 17:46:04 +0100

    Added cors

--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@
 
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 const port = process.env.PORT || 1233;
 
 const routes = require('./routes');
 
+app.use(cors());
 app.use('/', routes);
 
 app.listen(port)
