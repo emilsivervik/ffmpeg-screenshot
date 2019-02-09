974d6b015ebbd72692b2cabcaeb65d7404c0b744
Emil <emil.sivervik@kit.se>
2019-02-09 16:17:03 +0100

    Removed compression test

--- src/app.js
+++ src/app.js
@@ -3,13 +3,11 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const compression = require('compression');
 
 const port = process.env.PORT || 1233;
 
 const routes = require('./routes');
 
-app.use(compression())
 app.use(cors());
 app.use('/', routes);
 
---
+++ test/stresstest.js
@@ -0,0 +1,12 @@
+const request = require('request');
+
+const iterations = [100];
+console.log(iterations.length)
+const test = async () => {
+    
+}
+
+test()
+.catch(err => {
+    console.error(err)
+})
