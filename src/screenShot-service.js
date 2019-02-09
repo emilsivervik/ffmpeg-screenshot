b9e7f074871262d60eacb3b4dfd1bbedff8b4bb6
Emil <emil.sivervik@kit.se>
2019-02-09 16:14:15 +0100

    Refactored loads of stuff

--- src/screenShot-service.js
+++ src/screenShot-service.js
@@ -4,78 +4,34 @@ const express = require('express');
 const router = express.Router();
 
 const ffmpegStatic = require('ffmpeg-static');
-//const ffmpeg = require('fluent-ffmpeg');
+
 const { spawn } = require('child_process');
 
-//ffmpeg.setFfmpegPath(ffmpegStatic.path);
-
-const fs = require('fs')
-const path = require('path')
-const tmpFolderPath = path.join(__dirname, '/../tmp');
-const tmpExist = fs.existsSync(tmpFolderPath);
-if (!tmpExist) { fs.mkdirSync(tmpFolderPath) }
-
 router.get('/', (req, res, next) => {
-    const fileName = `${Date.now().toString()}.png`;
 
     const {
-        timestamp = '00:00:00.111',
+        timestamp = '00:00:00',
         size = '270x270',
         url
     } = req.query;
 
-    const options = {
-        timemarks: [timestamp],
-        size,
-        filename: fileName,
-        folder: tmpFolderPath,
-        fastSeek: true
-    }
-
-    const [width, height] = size.split('x');
 
     var ffmpeg = spawn(ffmpegStatic.path, [
-        '-i', url,
+        /* Should be left in -ss 1;th and -i 2;th order since we would like to do a quick Input seek */
         '-ss', timestamp,
+        '-i', url,
         '-vframes', 1,
-        '-vf', `scale=${width}:${height}`,
-        '-c:v', 'png',
+        '-s', size,
+        '-vcodec', 'mjpeg',
+        '-huffman', 'optimal',
         '-f', 'image2pipe',
-        'pipe:1'              // Output on stdout
+        'pipe:1'
     ]);
 
-    // pipe data to AirTunes
-    ffmpeg.stdout.pipe(res)
-    ffmpeg.on('err', err => {
+    ffmpeg.stdout.pipe(res);
+    ffmpeg.stdout.on('err', err => {
         return res.status(500).send();
-    })
-    /* ffmpeg(url)
-        .on('end', () => {
-            const filePath = path.join(tmpFolderPath, fileName);
-            const fileExist = fs.existsSync(filePath);
-            if (!fileExist) {
-                console.error('Could not find file, something wrong with inputs.');
-                return res.status(500).send();
-            }
-            fs.createReadStream(filePath)
-                .on('end', () => {
-                    fs.unlink(filePath, (err) => { if (err) console.log(err) });
-                })
-                .on('error', err => {
-                    const exist = fs.existsSync(filePath);
-                    if (exist) {
-                        fs.unlink(filePath, (err) => { if (err) console.log(err) });
-                    }
-                    console.error(err);
-                    return res.status(500).send();
-                })
-                .pipe(res)
-        })
-        .screenshots(options)
-        .on('error', err => {
-            console.error(err);
-            return res.status(500).send();
-        }) */
+    });
 })
 
 module.exports = router;
